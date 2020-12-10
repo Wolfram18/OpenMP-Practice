c4b961ad540141f2a582b5e8c6f1429d99385621
Wolfram18 <56358902+Wolfram18@users.noreply.github.com>
2020-12-10 08:25:08 +0300

    Refactor UseOfConditions

--- OpenMP-Project/Project2/UseOfConditions.cpp
+++ OpenMP-Project/Project2/UseOfConditions.cpp
@@ -1,10 +1,10 @@
 #include <omp.h>
-#include <chrono>
 #include <iostream>
+#include <ctime>
 using namespace std;
 
-const int NMAX = 50 - 1;
-const int LIMIT = 50;
+const int NMAX = 7500; //50 - 1;
+const int LIMIT = 10000;
 
 float a[NMAX][NMAX];
 
@@ -13,15 +13,16 @@ float a[NMAX][NMAX];
 void main()
 {
     srand(time(0));
+    clock_t start, stop;
     int i, j;
     float sum;
 
     for (i = 0; i < NMAX; i++)
         for (j = 0; j < NMAX; j++)
-            a[i][j] = rand() % 20;
+            a[i][j] = i + j;
 
     // Параллельный фрагмент
-    auto start = chrono::system_clock::now();
+    start = clock();
 #pragma omp parallel if (NMAX > LIMIT)
     {
 #pragma omp for private(i,j,sum) 
@@ -33,12 +34,10 @@ void main()
             //cout << "  Line: " << i << " --- Sum: " << sum << endl;
         }
     }
-    auto stop = chrono::system_clock::now();
-
-    chrono::duration<double> elaps = (stop - start);
+    stop = clock();
 
     if (NMAX > LIMIT)
-        cout << "\n  Time of Parallel: " << elaps.count() << endl << endl;
+        cout << "\n  Time of Parallel: " << (stop - start) << endl << endl;
     else
-        cout << "\n  Time of NoParallel: " << elaps.count() << endl << endl;
+        cout << "\n  Time of NoParallel: " << (stop - start) << endl << endl;
 }
